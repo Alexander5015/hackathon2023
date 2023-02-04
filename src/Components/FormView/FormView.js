d7c37b62f9a89255a24a66cb7e03413add7d9ce8
bunny2030 <vaibhav.kumar@edu.sait.ca>
2023-02-04 14:05:23 -0700

    login css

--- src/Components/FormView/FormView.js
+++ src/Components/FormView/FormView.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./FormView.css"
 
-function FormView({formHead, formFoot, formData}) {
+function FormView({ formHead, formFoot, formData }) {
     return (
         <div>
             <div className="formBackground">
                 <h2 className="formHeading ">{formHead}</h2>
-
+                <br></br>
                 <form>
                     {formData.inputs.map((input) => (
                         <div key={input.key}>
@@ -17,25 +17,25 @@ function FormView({formHead, formFoot, formData}) {
                                 placeholder={input.placeholder}
                                 onChange={input.handler}
                                 className="inputField"
-                            />
+                            /><br></br>
                         </div>
                     ))}
                     <div>
-                        <input type="submit" value={formHead} />
+                        <input type="submit" value={formHead} className="formSubmit" />
                     </div>
                     {formHead === "Login" && (
                         <div>
-                            <div>
-                                <input type="checkbox" name="rememberMe"/>
-                                Remember Me
+                            <div className="checkbox-circle">
+                                <input type="checkbox" id="checkbox-circle1" name="rememberMe"></input>
+                                    <label for="checkbox-circle1">Remember Me</label>
                             </div>
-                            <Link to={"/forgotpassword"} target="_blank">
+                            <Link to={"/forgotpassword"} target="_blank" className="formLastLinks">
                                 Forgot password?
                             </Link>
                         </div>
                     )}
                     <div>
-                        <Link to={formFoot.link}>{formFoot.name}</Link>
+                        <Link to={formFoot.link} className="formLastLinks">{formFoot.name}</Link>
                     </div>
                 </form>
             </div>
