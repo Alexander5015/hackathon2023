5966c69765570daa98d12dee34269c63893b3293
Patrick O <patrick.orihuela@gmail.com>
2023-02-05 11:11:04 -0700

    final commit

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -15,12 +15,22 @@ const Profile = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.empty}>
-        {/* Empty div */}
-      </div>
+      <div className={styles.empty}>{/* Empty div */}</div>
       <UserInfo />
-      <div className={styles.text}>
-        <h3 className={styles.title}>Shared Activities: </h3>
+
+      <div className="tabs">
+        <button
+          className={` ${activeTab === "tab1" && "active"}`}
+          onClick={() => handleTabClick("tab1")}
+        >
+          Shared Activities
+        </button>
+        <button
+          className={`tab-btn ${activeTab === "tab2" && "active"}`}
+          onClick={() => handleTabClick("tab2")}
+        >
+          Reviews
+        </button>
       </div>
 
       <div className="tab-content">
@@ -70,17 +80,37 @@ const Profile = () => {
               </button>
             </div>
           </div>
-          <h4 className={styles.reviewTitle}>Joe Doen</h4>
-          <StarIcon />
-        </div>
-        <div className={styles.bottom}>
-          <p className={styles.reviewDesc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio.
-          </p>
-        </div>
+        )}
+        {activeTab === "tab2" && (
+          <div>
+            <div className={styles.reviewContainer}>
+              <h3 className={styles.title}>Reviews: </h3>
+              <div className={styles.top}>
+                <div className={styles.reviewImage}>
+                  <img
+                    className={styles.userImage}
+                    src="/user1.jpg"
+                    alt="user1"
+                  />
+                </div>
+                <h4 className={styles.reviewTitle}>Joe Doen</h4>
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+              </div>
+              <div className={styles.bottom}>
+                <p className={styles.reviewDesc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                  consequuntur voluptatum laborum numquam blanditiis harum
+                  quisquam eius sed odit fugiat iusto fuga praesentium optio.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
