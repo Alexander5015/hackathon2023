6aa0b49383bbb734d576ff4451f93f7898916f44
Patrick O <patrick.orihuela@gmail.com>
2023-02-04 18:19:44 -0700

    Profile Page

--- src/Components/UserInfo/UserInfo.js
+++ src/Components/UserInfo/UserInfo.js
@@ -3,23 +3,36 @@ import styles from "../../styles/UserInfo.module.css";
 
 const UserInfo = ({ name, contact, about, image }) => {
   return (
-    <div className={styles.userInfo}>
+    <div className={styles.container}>
+      {/* //top */}
       <div className={styles.top}>
         <div className={styles.right}>
-          <img src="/user1.jpg" alt="not-found" className={styles.image} />
+          <div className={styles.imageContainer}>
+            <img src="/user1.jpg" alt="not-found" className={styles.image} />
+          </div>
+          <div className={styles.userDesc}>
+            <h3 className={styles.name}>Joe Doe</h3>
+            <p className={styles.contact}>123-456-7891</p>
+          </div>
         </div>
         <div className={styles.left}>
-          <h3 className={styles.name}>Joe Doe</h3>
-          <p className={styles.contact}>123-456-7891</p>
+          <div></div>
         </div>
       </div>
+      {/* //bottom */}
       <div className={styles.bottom}>
-        <p className={styles.about}>
+        <div className={styles.title}>
+          <h3>About me:</h3>
+        </div>
+        <div className={styles.containerAbout}>
+
+        </div>
+        <div className={styles.aboutDesc}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
-        </p>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import styles from "./../styles/Profile.module.css";
-import PersonIcon from '@mui/icons-material/Person';
+import PersonIcon from "@mui/icons-material/Person";
 import UserInfo from "../Components/UserInfo/UserInfo";
+import AddIcon from '@mui/icons-material/Add';
+
 const Profile = () => {
   return (
     <div className={styles.container}>
-      <div>
-        <UserInfo/>
+      <UserInfo />
+      <div className={styles.text}>
+        <h3 className={styles.title}>Past Trips:</h3>
+      </div>
+      <div className={styles.imagesContainer}>
+        <img src="/nature1.jpeg" alt="not-found" className={styles.image}></img>
+        <img src="/nature2.jpeg" alt="not-found" className={styles.image}></img>
+        <img src="/nature3.jpeg" alt="not-found" className={styles.image}></img>
+        <img src="/nature4.jpeg" alt="not-found" className={styles.image}></img>
+        <img src="/nature5.jpeg" alt="not-found" className={styles.image}></img>
+        <img src="/nature6.jpeg" alt="not-found" className={styles.image}></img>
+      </div>
+      <div class={styles.button}>
+        <AddIcon/>
+        <p>Show more ...</p>
       </div>
     </div>
   );
