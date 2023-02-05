84fb9b9e60ef8bfc741a56e2d241e59b56db5601
Patrick O <patrick.orihuela@gmail.com>
2023-02-05 10:07:15 -0700

    Profile page added

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,50 +3,115 @@ import styles from "./../styles/Profile.module.css";
 import PersonIcon from "@mui/icons-material/Person";
 import UserInfo from "../Components/UserInfo/UserInfo";
 import AddIcon from "@mui/icons-material/Add";
-import StarRateIcon from '@mui/icons-material/StarRate';
+import StarRateIcon from "@mui/icons-material/StarRate";
 
+import TabMenu from "../Components/TabMenu/TabMenu";
+import { useState } from "react";
 const Profile = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className={styles.container}>
       <UserInfo />
-      <div className={styles.text}>
-        <h3 className={styles.title}>Shared Activities: </h3>
+
+      <div className="tabs">
+        <button
+          className={` ${activeTab === "tab1" && "active"}`}
+          onClick={() => handleTabClick("tab1")}
+        >
+          Shared Activities
+        </button>
+        <button
+          className={`tab-btn ${activeTab === "tab2" && "active"}`}
+          onClick={() => handleTabClick("tab2")}
+        >
+          Reviews
+        </button>
       </div>
-      <div className={styles.imagesContainer}>
-        <img src="/nature1.jpeg" alt="not-found" className={styles.image}></img>
-        <img src="/nature2.jpeg" alt="not-found" className={styles.image}></img>
-        <img src="/nature3.jpeg" alt="not-found" className={styles.image}></img>
-        <img src="/nature4.jpeg" alt="not-found" className={styles.image}></img>
-        <img src="/nature5.jpeg" alt="not-found" className={styles.image}></img>
-        <img src="/nature6.jpeg" alt="not-found" className={styles.image}></img>
-      </div>
-      <div className={styles.button}>
-        <span className={styles.button}>
-          <AddIcon className={styles.addIcon} />
-        </span>
-        <p>Share more ...</p>
-      </div>
-      <h3 className={styles.title}>Reviews: </h3>
-      <div className={styles.reviewContainer}>
-        <div className={styles.top}>
-          <div className={styles.reviewImage}>
-            <img className={styles.userImage} src="/user1.jpg" alt="user1" />
+
+      <div className="tab-content">
+        {activeTab === "tab1" && (
+          <div>
+            <div className={styles.text}>
+              <h3 className={styles.title}>Shared Activities: </h3>
+            </div>
+            <div className={styles.imagesContainer}>
+              <img
+                src="/yyc/yyc.jpeg"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+              <img
+                src="/yyc/yyc2.jpeg"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+              <img
+                src="/yyc/yyc3.jpeg"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+              <img
+                src="/yyc/yyc4.webp"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+              <img
+                src="/yyc/yyc5.webp"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+              <img
+                src="/yyc/yyc6.jpg"
+                alt="not-found"
+                className={styles.image}
+              ></img>
+            </div>
+            <div className={styles.buttonContainer}>
+              <button
+                className={` ${activeTab === "tab1" && "active"}`}
+                onClick={() => handleTabClick("tab1")}
+              >
+                Share more
+                <AddIcon className={styles.addIcon} />
+              </button>
+            </div>
           </div>
-          <h4 className={styles.reviewTitle}>Joe Doen</h4>
-          <StarRateIcon />
-          <StarRateIcon />
-          <StarRateIcon />
-          <StarRateIcon />
-          <StarRateIcon />
-        </div>
-        <div className={styles.bottom}>
-          <p className={styles.reviewDesc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio.
-          </p>
-        </div>
+        )}
+        {activeTab === "tab2" && (
+          <div>
+            <div className={styles.reviewContainer}>
+              <h3 className={styles.title}>Reviews: </h3>
+              <div className={styles.top}>
+                <div className={styles.reviewImage}>
+                  <img
+                    className={styles.userImage}
+                    src="/user1.jpg"
+                    alt="user1"
+                  />
+                </div>
+                <h4 className={styles.reviewTitle}>Joe Doen</h4>
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+                <StarRateIcon />
+              </div>
+              <div className={styles.bottom}>
+                <p className={styles.reviewDesc}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                  consequuntur voluptatum laborum numquam blanditiis harum
+                  quisquam eius sed odit fugiat iusto fuga praesentium optio.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
